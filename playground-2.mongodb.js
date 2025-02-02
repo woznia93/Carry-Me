26dc77de3294800393b19863ced716b7a5e349d9
DanielPetk <danielpetkoski@hotmail.com>
2025-02-02 06:36:26 -0500

    messing with some stuff

--- playground-2.mongodb.js
+++ playground-2.mongodb.js
@@ -5,7 +5,7 @@
 // Use Ctrl+Space inside a snippet or a string literal to trigger completions.
 // The result of the last command run in a playground is shown on the results panel.
 // By default the first 20 documents will be returned with a cursor.
-// Use 'console.log()' to print to the debug output.
+// Use 'console.log()' to print to the debug output..
 // For more documentation on playgrounds please refer to
 // https://www.mongodb.com/docs/mongodb-vscode/playgrounds/
 
