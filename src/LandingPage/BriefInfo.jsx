80972b6d53f86970070e3db3ab42c4e5458cd418
Brady <woznia93@msu.edu>
2025-02-01 23:33:19 -0500

    message updated

--- src/LandingPage/BriefInfo.jsx
+++ src/LandingPage/BriefInfo.jsx
@@ -6,7 +6,7 @@ function BriefInfo() {
         <div className=" text-white text-center leading-snug container mt-6 overflow-visible px-12 font-jetbrains">
             <div className='w-[100%] overflow-visible flex flex-col items-center'>
                 <h2 className='text-4xl text-center font-bold bg-gradient-to-r overflow-visible leading-20 text-white container'>
-                Test and testing description testing description testing description testing description
+                Want to get better at a game? Play with lower ranked players and carry them to victory to gain elo and join the leaderboards.
                 </h2>
             </div>
         </div>
